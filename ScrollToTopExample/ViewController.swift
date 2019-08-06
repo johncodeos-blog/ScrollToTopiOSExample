b15a5de7336b4ebab2065d5d5d16bc79b19901f0
John <johncodeos@gmail.com>
2019-08-06 18:58:13 +0300

    Initial Commit

---
+++ ScrollToTopExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollToTopExample
+//
+//  Created by John Codeos on 06/08/2019.
+//  Copyright © 2019 John Codeos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
